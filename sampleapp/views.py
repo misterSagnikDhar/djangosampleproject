748aa56cebd8dc228680daa0a8827843bca9fac2
misterSagnikDhar <sagnikdhar03@gmail.com>
2025-03-04 14:17:11 +0530

    added hello world on homepage

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return request
+def hello_world(request):
+    return HttpResponse("Hello World!")
